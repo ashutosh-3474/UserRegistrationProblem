589c1a1cbc61e0bc2b7f7fe9c577da287ad0de24
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-17 16:02:25 +0530

    Added: logic to validate first name using regex

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the program for user registration and validation");
+
+        RegexValidator regexValidator = new RegexValidator();
+        //Check for validation of first name
+        System.out.println("First Name");
+        System.out.println("Ashutosh-> " + (regexValidator.validateFirstName("Ashutosh") ? "Valid" : "Invalid"));
+        System.out.println("ashutosh-> " + (regexValidator.validateFirstName("ashutosh") ? "Valid" : "Invalid"));
+        System.out.println("a-> " + (regexValidator.validateFirstName("a") ? "Valid" : "Invalid"));
+        System.out.println("AshuH-> " + (regexValidator.validateFirstName("AshuH") ? "Valid" : "Invalid"));
+        System.out.println();
+
     }
 }
---
+++ src/RegexValidator.java
@@ -0,0 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//Custom class for regex pattern and matcher
+public class RegexValidator {
+    //Regex pattern for first name
+    private static final String  FirstNameRegex = "^[A-Z][a-zA-Z]{2,}$";
+
+    //Method to validate the input with the regex pattern
+    private static boolean validate(String input, String regex)
+    {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    //Method to validate first name
+    public static boolean validateFirstName(String firstName) {
+        return validate(firstName, FirstNameRegex);
+    }
+}
